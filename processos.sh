a9ef17a19f7ba03292456412668dd0c79786b3ce
alissonwegner <alissonwegner@hotmail.com>
2021-03-31 16:03:19 -0300

    up

--- processos.sh
+++ processos.sh
@@ -5,3 +5,9 @@ for pid in $processos
 do
     echo $(ps -p $pid -o comm=)
 done
+
+
+#para salvar em um arquivo as informações
+tamanho_processo=$(ps -p $pid -o size | grep [0-9])
+echo -n $(date +%F,%H:%M:%S,) >> log/$nome_processo.log
+echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> $nome_processo.log
